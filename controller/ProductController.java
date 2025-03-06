419f4d2e133eb0c8e0e66e9de0ab7862e8460b79
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2025-03-06 23:22:55 +0200

    Update and Delete Basics completed

--- controller/ProductController.java
+++ controller/ProductController.java
@@ -3,9 +3,11 @@ package com.Moloko.simpleWebAppp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +36,16 @@ public class ProductController
 	{
 		System.out.println(prod);
 		service.addProduct(prod);
+	}
+	@PutMapping("/products") //Updating JSON data
+	public void updateProduct(@RequestBody Product prod)
+	{
+		System.out.println("im in update");
+		service.updateProduct(prod);
+	}
+	@DeleteMapping("/products/{prodId}")
+	public void deleteProduct(@PathVariable int prodId)
+	{
+		service.deleteProduct(prodId);
 	}
 }
